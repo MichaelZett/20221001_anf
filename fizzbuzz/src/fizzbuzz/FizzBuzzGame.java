f1a6388ed038d34b12890d2ad4719d4f7659f033
MichaelZett <michael2.zoeller@gmail.com>
2022-10-23 11:12:18 +0200

    Update FizzBuzzGame.java
    Use constants

--- fizzbuzz/src/fizzbuzz/FizzBuzzGame.java
+++ fizzbuzz/src/fizzbuzz/FizzBuzzGame.java
@@ -2,24 +2,25 @@ package fizzbuzz;
 
 public class FizzBuzzGame {
 
+	private static final int DEFAULT_LAST_NUMBER = 100;
+	private static final int FIZZ_NUMBER = 3;
+	private static final int BUZZ_NUMBER = 5;
+
 	public static void main(String[] args) {
-		int lastNumber = 100;
+		int lastNumber = DEFAULT_LAST_NUMBER;
 		if (args.length > 0) {
 			try {
 				lastNumber = Integer.parseInt(args[0]);
 			} catch (NumberFormatException e) {
-				System.out.println(
-						"Als Programmargument wird eine Zahl erwartet." + " Ich mache mit default 100 weiter.");
+				System.out.println("Als Programmargument wird eine Zahl erwartet. Ich mache mit default 100 weiter.");
 			}
 		}
-		int fizzNumber = 3;
-		int buzzNumber = 5;
 		for (int i = 1; i <= lastNumber; i++) {
-			if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+			if (i % FIZZ_NUMBER == 0 && i % BUZZ_NUMBER == 0) {
 				System.out.print("FizzBuzz");
-			} else if (i % fizzNumber == 0) {
+			} else if (i % FIZZ_NUMBER == 0) {
 				System.out.print("Fizz");
-			} else if (i % buzzNumber == 0) {
+			} else if (i % BUZZ_NUMBER == 0) {
 				System.out.print("Buzz");
 			} else {
 				System.out.print(i);
